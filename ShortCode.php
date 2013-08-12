00b10752f0de16efaf4e94b7af3a86dd05fe1f0b
K.Sasaki <among753@gmail.com>
2013-08-12 18:33:45 +0900

    Edit Shortcode

--- ShortCode.php
+++ ShortCode.php
@@ -37,27 +37,7 @@ class ShortCode {
 
 
 
-		add_shortcode('sns-trend-list', array($this, 'snsTrendList'));
-	}
-
-	public function snsTrendList($atts) {
-		global $post;
-		$post_id   = $post->ID;
-		$trend_type = 'twitter';
-		$count = 10;
-		extract(shortcode_atts(
-			array(
-				'post_id'    => $post_id,
-				'trend_type' => $trend_type,
-				'count'      => $count
-			),
-			$atts
-		));
-
-		// TODO post_type post_id で絞り込み検索
-		// TODO Ajaxで読み込みを実装
-		$this->showTrendList(20);
-
+		add_shortcode('sns-trend-list', array($this, 'setSnsTrendList'));
 	}
 
 	/**
@@ -66,50 +46,66 @@ class ShortCode {
 	 * "wp_ajax_hook_name" のアクションフックに登録してあるfunctionを実行する
 	 * 実行されたfunctionの出力データを success:function(data){} で受け取り処理を行う
 	 */
-	public function showTrendList( $limit=30 ) {
+	public function setSnsTrendList( $atts ) {
 		global $post, $wp_query;
 
+		$post_id   = $post->ID;
+		$trend_type = Twitter::TYPE;
+		$limit = 10;
+		extract(shortcode_atts(
+			array(
+				'post_id'    => $post_id,
+				'trend_type' => $trend_type,
+				'limit'      => $limit
+			),
+			$atts
+		));
+
+
 		if ( is_archive() ) {
-			echo "trendのカウントでも表示しとく？";
-//			var_dump($post, $wp_query);
-			return false;
+			// 表示件数を減らす
+			$limit = 3;
 		}
-
-
+		$show_id   = "show-trend-list-" . $post_id;
+		$form_id   = "load-trend-list" . $post_id;
+		$submit_id = "load-trend-list-submit-" . $post_id;
 		?>
-		<div id="show-trend-list">
-			<p>ひょうじするとこーーーーー</p>
+		<div id="<?php esc_attr_e($show_id); ?>">
+			<p>Twitter list</p>
 		</div>
-		<form action="" name="load-trend-list">
+		<form action="" name="<?php esc_attr_e($form_id); ?>">
 			<?php wp_nonce_field( self::ACTION ); ?>
 			<input type="hidden" name="limit" id="limit" value="<?php esc_attr_e($limit); ?>">
 			<input type="hidden" name="offset" id="offset" value="0">
-			<input type="submit"  name="load-trend-list-submit" id="load-trend-list-submit"value="<?php _e("更に読み込む", SnsTrend::NAME_DOMAIN);?>">
+			<input type="submit"  name="<?php esc_attr_e($submit_id); ?>" id="<?php esc_attr_e($submit_id); ?>"value="<?php _e("更に読み込む", SnsTrend::NAME_DOMAIN);?>">
 		</form>
 		<script type="text/javascript">
 			ajaxurl = '<?php echo admin_url( 'admin-ajax.php' ); ?>';
 			jQuery(function($){
 				// #TODO clickをやめて画面下にスクロールしたら読み込みに変更
-				$("#load-trend-list-submit").click(function(){
+				$(<?php echo "'#".esc_attr($submit_id)."'"; ?>).click(function(){
 					$.ajax({
 						type: 'POST',
 						url: ajaxurl,
 						data: {
-							"action" : "<?php echo self::ACTION; ?>",// action hook
-							"post_id" : "<?php echo esc_js($post->ID); ?>",
-							"limit" : $(this.form).find("input[name='limit']").val(),
-							"offset" : $(this.form).find("input[name='offset']").val(),
-							"_wpnonce" : $(this.form).find("input[name='_wpnonce']").val(),
+							"action"           : "<?php echo self::ACTION; ?>",// action hook
+							"post_id"          : "<?php echo esc_js($post_id); ?>",
+							"limit"            : $(this.form).find("input[name='limit']").val(),
+							"offset"           : $(this.form).find("input[name='offset']").val(),
+							"_wpnonce"         : $(this.form).find("input[name='_wpnonce']").val(),
 							"_wp_http_referer" : $(this.form).find("input[name='_wp_http_referer']").val()
 						},
 						success: function(json){
 							// php処理成功後
 //							var json_str = JSON.stringify(json);//Jsonデータを文字列に変換
-							$('#show-trend-list').append(json.data);
+							$(<?php echo "'#".esc_attr($show_id)."'"; ?>).append(json.data);
 							// offsetをlimit分増やす
-							$("input[name='offset']").val(Number(json.offset) + Number(json.limit));
+							$("input[name='offset']").val(Number(json.offset) + Number(json.limit));// TODO 実liの数を数えた方がいいかも
 
-							$("input[name='load-trend-list-submit']").css({
+							// ボタン処理
+							$submit_button = $(<?php echo "'#".esc_attr($submit_id)."'"; ?>);
+							$submit_button.css({
+								<?php if ( is_archive() ) echo "'display': 'none',";/*アーカイブページのときボタンけす*/ ?>
 								'pointerEvents': 'auto',
 								'color': '#000'
 							});
@@ -119,6 +115,7 @@ class ShortCode {
 						}
 					});
 					// click後のJavaScript処理
+					// ボタン処理
 					$(this).css({
 						'pointerEvents': 'none',
 						'color': '#ccc'
@@ -161,23 +158,9 @@ class ShortCode {
 		}
 		$args['data'] = $data;
 
-
 		nocache_headers();
 		header( "Content-Type: application/json; charset=" . get_bloginfo( 'charset' ) );
-
 		die( json_encode($args) );
 	}
 
-}
-
-
-/**
- * unserializeしたいとき文字コードが違う場合などに
- *
- * @param $string
- * @return mixed
- */
-function _unserialize($string) {
-	$ret = preg_replace('!s:(\d+):"(.*?)";!e', "'s:'.strlen('$2').':\"$2\";'", $string);
-	return unserialize($ret);
 }
